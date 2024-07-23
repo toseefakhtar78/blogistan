b7cadf9bcea119827681851a435c4e06f3e5e6a0
waheed <waheed.jutt2323@gmail.com>
2024-07-23 14:45:14 +0500

    new changes

--- components/layout.js
+++ components/layout.js
@@ -53,9 +53,22 @@ export default function Layout({ children, home }) {
           </>
         )}
       </header>
+
       <main>{children}</main>
       {!home && (
         <div className={styles.backToHome}>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1241486495309147"
+     crossorigin="anonymous"></script>
+
+<ins class="adsbygoogle"
+     style={{display:"block"}}
+     data-ad-client="ca-pub-1241486495309147"
+     data-ad-slot="6780024865"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
           <Link href="/">← Back to home</Link>
         </div>
       )}
